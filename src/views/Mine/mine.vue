2c000c504b5d64d982154eeb31bb3b40617b9260
3117005228 <745188905@qq.com>
2020-09-23 23:59:31 +0800

    add createcomponents

---
+++ src/views/Mine/mine.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id='main'>
+     <Header title='我的'/>
+	 <div id="content">
+	 	<login/>
+	 </div>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+  import Header from 'components/Header'
+  import TabBar from 'components/TabBar'
+  import Login from 'components/Login/login'
+	
+  export default {
+    name: "mine",
+	components: {
+		Header,
+		TabBar,
+		Login
+	}
+  }
+</script>
+
+<style scoped="scoped">
+</style>
